15b55a720d6d917383dad310fbe8494e6e919635
Rabits <home@rabits.org>
2013-02-14 00:36:58 +0400

    * Prepared Data2sd configuration

--- tools/prepare_installer.sh
+++ tools/prepare_installer.sh
@@ -8,7 +8,8 @@
 
 OUTPUT="$1"
 BOOTBLOB="$3"
-BUSYBOX="$4"
+BOOTBLOBD2SD="$4"
+BUSYBOX="$5"
 
 DIST_VER=$(git tag | tail -n1)
 DIST_DATE=$(date "+%d/%m/%Y")
@@ -25,6 +26,7 @@ else
 fi
 
 [ "x${BOOTBLOB}" = "x" ] && BOOTBLOB="boot/img/boot.blob"
+[ "x${BOOTBLOBD2SD}" = "x" ] && BOOTBLOBD2SD="boot/img/boot-data2sd.blob"
 [ "x${BUSYBOX}" = "x" ] && BUSYBOX="source/busybox/out/_install/bin/busybox"
 
 if [ "x${OUTPUT}" = "x" ]; then
@@ -35,24 +37,30 @@ fi
 touch "${OUTPUT}"
 if [ ! -w "${OUTPUT}" ]; then
     echo "E: To create installer, please specify writable output file, ${OUTPUT} - not found." 1>&2
-    echo "Usage: $0 <output_file.zip> [boot.blob] [busybox]" 1>&2
+    echo "Usage: $0 <output_file.zip> [boot.blob] [boot-data2sd.blob] [busybox]" 1>&2
     rm -f "${OUTPUT}"
     exit 1
 fi
 rm -f "${OUTPUT}"
 if [ ! -r "${BOOTBLOB}" ]; then
     echo "E: To create installer, please specify readable output file, ${BOOTBLOB} - not found." 1>&2
-    echo "Usage: $0 <output_file.zip> [boot.blob] [busybox]" 1>&2
+    echo "Usage: $0 <output_file.zip> [boot.blob] [boot-data2sd.blob] [busybox]" 1>&2
+    exit 1
+fi
+if [ ! -r "${BOOTBLOBD2SD}" ]; then
+    echo "E: To create installer, please specify readable output file, ${BOOTBLOBD2SD} - not found." 1>&2
+    echo "Usage: $0 <output_file.zip> [boot.blob] [boot-data2sd.blob] [busybox]" 1>&2
     exit 1
 fi
 if [ ! -r "${BUSYBOX}" ]; then
     echo "E: To create installer, please specify readable output file, ${BUSYBOX} - not found." 1>&2
-    echo "Usage: $0 <output_file.zip> [boot.blob] [busybox]" 1>&2
+    echo "Usage: $0 <output_file.zip> [boot.blob] [boot-data2sd.blob] [busybox]" 1>&2
     exit 1
 fi
 
 OUTPUT=`readlink -f "${OUTPUT}"`
 BOOTBLOB=`readlink -f "${BOOTBLOB}"`
+BOOTBLOBD2SD=`readlink -f "${BOOTBLOBD2SD}"`
 BUSYBOX=`readlink -f "${BUSYBOX}"`
 
 
@@ -66,6 +74,8 @@ sed -i "s|ini_set(\"rom_version\".*|ini_set(\"rom_version\", \"${DIST_VER}\");|g
 
 echo -n "I: Linking ${BOOTBLOB} as boot.blob file into archive..."
 ln -sf "${BOOTBLOB}" installer/boot.blob && echo " ok" || echo " fail"
+echo -n "I: Linking ${BOOTBLOBD2SD} as boot-data2sd.blob file into archive..."
+ln -sf "${BOOTBLOBD2SD}" installer/boot-data2sd.blob && echo " ok" || echo " fail"
 
 echo -n "I: Linking ${BUSYBOX} as busybox file into archive..."
 ln -sf "${BUSYBOX}" installer/META-INF/com/google/android/aroma/exec/busybox && echo " ok" || echo " fail"
